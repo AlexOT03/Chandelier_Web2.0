cb3d00a1e0843be659197f5543bb61f8752024be
Jesus <alexot855@gmail.com>
2023-06-12 11:24:26 -0600

    Chandelier Web | Reboot de la aplicacion web

---
+++ chandelier_web/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from chandelier_web.users.models import User
+from chandelier_web.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
